6a20b7802deaf11e5f89e7747706f0ca39d61dd5
Alan Quintana <alanqff@gmail.com>
2026-05-01 18:03:31 -0600

    variables

--- app-consultorio/src/pages/Home.tsx
+++ app-consultorio/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import {staticbackend} from "@/config/variables";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
           </div>
         </div>
       </nav>
+      <p>{staticbackend}</p>
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <section className="text-center mb-12">
